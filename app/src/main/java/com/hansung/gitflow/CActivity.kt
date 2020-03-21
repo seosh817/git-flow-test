71a1e5b91a721b72af83fbba68ce1ff7d0452e21
seunghwan <slflfl12@naver.com>
2020-03-21 19:37:32 +0900

    #3 [c] c first commit

--- app/src/main/java/com/hansung/gitflow/CActivity.kt
+++ app/src/main/java/com/hansung/gitflow/CActivity.kt
@@ -1,12 +1,14 @@
 package com.hansung.gitflow
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class CActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(C, "C First Commit")
     }
 
     companion object{
