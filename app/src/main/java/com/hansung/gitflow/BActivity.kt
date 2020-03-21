e708d2cfe22abb6b11dca0b0d00c3c65229f05f7
seunghwan <slflfl12@naver.com>
2020-03-21 18:43:49 +0900

    #2 [b] b first commit

--- app/src/main/java/com/hansung/gitflow/BActivity.kt
+++ app/src/main/java/com/hansung/gitflow/BActivity.kt
@@ -1,11 +1,13 @@
 package com.hansung.gitflow
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class BActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(B, "B")
     }
 
     companion object{
