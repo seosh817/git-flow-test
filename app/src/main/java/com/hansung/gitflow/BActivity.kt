41fc7f50eeed63780d56ebef1080e1d21d1aee2e
seunghwan <slflfl12@naver.com>
2020-03-21 19:49:36 +0900

    #2 [b] b second commit

--- app/src/main/java/com/hansung/gitflow/BActivity.kt
+++ app/src/main/java/com/hansung/gitflow/BActivity.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 class BActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(B, "B")
+        Log.d(B, "B first commit")
+        Log.d(B, "B second commit")
+
     }
 
     companion object{
